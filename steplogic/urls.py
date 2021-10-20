59e75635cd00f49804a2fb93b9f91f96afb61ef0
Alex Araya <icommsoft@gmail.com>
2021-10-19 23:42:49 -0600

    UI Interface last Version

--- steplogic/urls.py
+++ steplogic/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/',views.login_view, name = 'login'),
     path('<str:site_name>/', views.detail, name='detail'),
     path('<str:site_name>/edit', views.edit, name='edit'),
     path('<str:site_name>/contacts', views.contacts, name='contacts'),
